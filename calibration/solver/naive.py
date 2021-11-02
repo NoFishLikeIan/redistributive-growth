4282142811716afcd3edea24d3aaa92783c39720
NoFishLikeIan <andreatitton96@gmail.com>
2021-11-02 09:55:23 +0100

    🎉🪐 newton solver

---
+++ calibration/solver/naive.py
@@ -0,0 +1,21 @@
+import scipy.optimize as opt
+
+from .model import Model
+
+
+def get_eta(model: Model, Y: float, H: float, K: float) -> float:
+    """
+    Numerically computes `eta` based on a `model` instance and a value of `Y`, `K` and, `H`. 
+
+    The computation is done numerically and not analytically to abstract from the output function.
+    """
+
+    def equilibrium(eta):
+
+        deltaY = Y - model.Y(H, K, eta)
+        deltaK = K - model.Ks(H, K, eta)
+        deltaH = H - model.Hs(H, K, eta)
+
+        return deltaY + deltaH + deltaK
+
+    return opt.newton(equilibrium, )
